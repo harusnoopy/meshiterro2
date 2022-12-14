4cd05b5e15d280cd7ab63d8b878a9613a4d2bf73
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-14 06:40:29 +0000

    finish 15

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
   
- resources :post_images, only: [:new, :create, :index, :show, :destroy
- ]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 end
