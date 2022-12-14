0e99f5d89cedcb53dcc91530ddb74c6cbd9b66f4
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-14 08:21:45 +0000

    finish 19

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
