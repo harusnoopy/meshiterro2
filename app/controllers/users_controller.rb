ea5aa22fec0def9b8ee7b826db4c1be8ccb5ed7f
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-14 07:08:27 +0000

    finish 16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
