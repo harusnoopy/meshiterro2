517c417aca7087832851a057a036ee01f87f9db4
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-14 14:39:42 +0000

    finish 21

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,9 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
   def get_image
     unless image.attached?
